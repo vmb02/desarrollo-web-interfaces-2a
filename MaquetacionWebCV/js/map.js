dba6830a4ec7a0ba9e27484ae20dd107bb1fa2ae
vmb02 <morenobenitezvictor@gmail.com>
2024-01-27 20:50:30 +0100

    Añado mejoras

--- MaquetacionWebCV/js/map.js
+++ MaquetacionWebCV/js/map.js
@@ -5,8 +5,7 @@ document.addEventListener("DOMContentLoaded", (event) =>{
     // genero un efecto de carga mediante un TimeOut 
     setTimeout(() =>{
        document.querySelector("#load-iframe-map").innerHTML=`
-       <iframe class="contact__iframe" frameborder="0" scrolling="no" marginheiht="0" marginwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.2996623515555!2d-4.462811723546774!3d36.69133637227841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f9c45623d2c3%3A0x7a634bd28c0cf953!2sMEDAC%20Nova%20%F0%9F%A5%87%20Centro%20Tecnol%C3%B3gico%20de%20FP!5e0!3m2!1ses!2ses!4v1698701252334!5m2!1ses!2ses"></iframe>
-    `; 
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3201.345917479334!2d-4.695803710980997!3d36.64212800516423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72e7014fe8afc1%3A0x3f56be321ea7f782!2sC.%20Convento%2C%2027%2C%2029120%20Alhaur%C3%ADn%20el%20Grande%2C%20M%C3%A1laga!5e0!3m2!1ses!2ses!4v1706383585856!5m2!1ses!2ses" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    `; 
     }, 500);
     
 });
